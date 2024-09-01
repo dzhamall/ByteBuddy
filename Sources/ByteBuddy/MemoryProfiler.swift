03214c7de4e9881603ab93e14d573c648778535b
Dzhamall <dzhamall@icloud.com>
2024-09-01 15:01:23 +0300

    Move to async await

--- Sources/ByteBuddy/MemoryProfiler.swift
+++ Sources/ByteBuddy/MemoryProfiler.swift
@@ -4,77 +4,37 @@
 import Foundation
 import Shared
 
-public final class MemoryProfiler {
-    private let session: URLSession
+typealias InternalError = ByteBuddy.Error
+
+public struct MemoryProfiler {
     private let env: Environment
 
     public init(env: Environment) {
-        self.session = URLSession(configuration: .default)
         self.env = env
     }
 
-    public func detectLeaks(completion: @escaping (Result<Void, ByteBuddy.Error>) -> Void) {
-        do {
-            try execute(.leaks) { [weak self] result in
-                guard let self else { return }
-                switch result {
-                case .success(let data):
-                    let result = self.prepareLeaksOutput(data)
-                    completion(result)
-
-                case .failure(let error):
-                    completion(.failure(error))
-                }
-            }
-        } catch let error as ByteBuddy.Error {
-            completion(.failure(error))
-        } catch {
-            Logger.log(error.localizedDescription, level: .error)
-            completion(.failure(.unknown))
-        }
+    public func detectLeaks() async throws -> LeaksOutput {
+        let data = try await execute(.leaks)
+        let leaksOutput = try Parser.parseLeaksOutput(data)
+        return leaksOutput
     }
 }
 
 // MARK: - Private
 
 private extension MemoryProfiler {
-    func execute(_ command: Command, completion: @escaping (Result<Data, ByteBuddy.Error>) -> Void) throws {
+    func execute(_ command: Command) async throws -> Data {
         let request = try command.asURLRequest(env: env)
-        let task = session.dataTask(with: request) { data, response, error in
-            if let error = error {
-                completion(.failure(.serviceError(error)))
-                return
-            }
+        let (data, response) = try await URLSession.shared.data(for: request)
 
-            guard let response = response as? HTTPURLResponse else {
-                completion(.failure(.noHttpResponse))
-                return
-            }
-
-            guard response.statusCode == 200 else {
-                completion(.failure(.unexpectedHttpStatusCode(response)))
-                return
-            }
-
-            guard let data = data else {
-                completion(.failure(.noData(response)))
-                return
-            }
-
-            completion(.success(data))
+        guard let response = response as? HTTPURLResponse else {
+            throw InternalError.noHttpResponse
         }
-        task.resume()
-    }
 
-    func prepareLeaksOutput(_ data: Data) -> Result<Void, ByteBuddy.Error> {
-        do {
-            let leaks = try Parser.parseLeaksOutput(data)
-            return leaks.count == 0 ? .success(()) : .failure(.leaksFoud(output: leaks))
-        } catch let error as ByteBuddy.Error {
-            return .failure(error)
-        } catch {
-            Logger.log(error.localizedDescription, level: .error)
-            return .failure(.unknown)
+        guard response.statusCode == 200 else {
+            throw InternalError.unexpectedHttpStatusCode(response)
         }
+
+        return data
     }
 }
